66a17956987d1b739a2e84fd65ea3868982c2209
joonsol <71648638+joonsol@users.noreply.github.com>
2025-05-23 00:22:37 +0000

    1

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
+# 타이틀
 st.title("Streamlit 자동 배포 테스트")
 
+# 문자열 입력
 name = st.text_input("이름을 입력하세요")
 
+age=st.number_input("나이를 입력하세요" ,min_value=0,max_value=100)
 
 if st.button("확인"):
-    st.success(f"{name}님 반가워요!")
+    st.success(f"{name}님 반가워요! 당신의 나이는 {age}세입니다.")
+
+
+
+
